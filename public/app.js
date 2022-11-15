fbb725ac28e23b9be2ce56e3dfa02e785f77e3a3
Sadiabehlim <sadiabehlim.123@gmail.com>
2022-11-15 07:29:39 -0800

    java started

---
+++ public/app.js
@@ -0,0 +1,317 @@
+alert ("Hi! welcome to javascript");
+alert ("Error! Please enter a valid password.");
+var message1 = "Welcome to JS land..."
+var message2 = "Happy Coding!"
+var message = message1+"\n"+message2
+alert(message);
+ alert ("Welcome to JS Land...");
+ var msg1 = "Happy Coding!"
+ var msg2 = "Prevent this page from creating additional dialogs"
+ var msg = msg1+"\n"+msg2;
+ alert (msg);
+ 
+ alert ("Hello...I can run JS through my web browser's console");
+ console.log("Hello...I can run JS through my web browser's console");
+// Chapter2
+var username = "Sadia";
+console.log(username);
+var myname = "Sadia";
+var fullname = myname + "behlim"
+console.log(fullname);
+var mesage;
+mesage = "Hello World";
+alert(mesage);
+var studentname = "John Doe";
+alert(studentname);
+var age = "15 years old";
+alert(age);
+var certificate = "Certified Mobile Application Development";
+alert(certificate);
+var lettera = "PIZZA";
+var letterb = "PIZZ";
+var letterc = "PIZ";
+var letterd = "PI";
+var lettere = "P";
+var alletters = lettera + "\n" + letterb +"\n" + letterc + "\n" + letterd + "\n" + lettere;
+alert(alletters);
+var e = " ";
+mail="sadu.memon@gmail.com";
+
+var email = "my email is" + e + mail;
+alert(email);
+var book = "A smarter way to learn Java Script";
+console.log(book);
+var line = "I am trying to learn from the Book A smarter"
+var line1 = "way to learn JavaScript";
+alert(line + "\n" + line1);
+document.write("yah!I can write html content through javascript")
+var dot = "---------------";
+var caption = "ஜ۩۞۩ஜ";
+alert ( dot + caption + dot);
+// chapter 3\\
+alert(age);
+var visit = "You have visited this site 14 times";
+alert(visit);
+
+document.write("<br> my birth year is 2005");
+document.write("<br> Data type of my declared variable is number <br>");
+var visitorname = "JohnDoe";
+var quantity = "5"
+var productname = "T-shirts"
+var clothing = "sania Clothing"
+document.write( visitorname + e +"ordered"+ e + quantity + e + productname  + e + "from" + e+ clothing );
+//Chapter 4 \\
+var numbers = "12 , 15 ,16";
+console.log(numbers);
+var legal = "letter , $_,apple1 , arrow , row";
+var legal1 = "these are legal variables :"
+console.log(legal1 + legal);
+var illegal = "these are illegal variables :";
+var illegal1 ="1apple , 11, <12, 2 arrow, 5mango ";
+console.log(illegal + illegal1);
+document.write("<br> variable names can only contain, numbers, $ and _.For example : $ my first Variable");
+document.write("<br> Variable must begin with a letter,$ or _ For example: $name_name or name");
+document.write("<br> Variable name should not be JS keywords");
+// chapter5 \\
+var a = 3;
+var b = 5;
+var sum = a + b;
+console.log(sum);
+document.write("<br> sum of 3 and 5 is 8");
+var difference = a - b;
+console.log(difference);
+document.write("<br> difference of 3 and 5 is -2");
+var product = a * b;
+console.log(product);
+document.write("<br> product of 3 and 5 is 15");
+var divide = a / b;
+console.log(divide);
+document.write("<br> when 3 is divided by 5 we get 0.6");
+var modulus = a % b;
+console.log(modulus);
+document.write("<br> the answer of modulus is 3");
+ 
+var value = undefined;
+document.write("<br> Value after variable declaration is undefined");
+var initialvalue  = 5;
+document.write("<br> INITIAL value : 5");
+var totalvalue = ++initialvalue;
+console.log(totalvalue);
+document.write("br>Value after increment is 6");
+var num = 7;
+var average = totalvalue + num;
+console.log(average);
+document.write("<br> Value after addition is 13 ");
+var decrease = --average;
+console.log(decrease);
+document.write("<br> Value after decrement is 12 ");
+var divide = decrease / 3;
+document.write("<br> The remainder is 0");
+var ticketprice = 600;
+var ticket = 5;
+var t0talticket= ticketprice * ticket;
+console.log(t0talticket);
+document.write("<br> Total cost to buy 5 tickets to a movie is 3000 PKR");
+document.write("<br>");
+ var table = 4;
+ var ans = table * 1;
+ document.write("<br> 4 X 1= 4");
+ var ans = table * 2;
+ document.write("<br> 4 X 2= 8");
+ var ans = table * 3;
+ document.write("<br> 4 X 3= 12");
+ var ans = table * 4;
+ document.write("<br> 4 X 4= 16");
+ var ans = table * 5;
+ document.write("<br> 4 X 5= 20");
+ var ans = table * 6;
+ document.write("<br> 4 X 6= 24");
+ var ans = table * 7;
+ document.write("<br> 4 X 7= 28");
+ var ans = table * 8;
+ document.write("<br> 4 X 8= 32");
+ var ans = table * 9;
+ document.write("<br> 4 X 9= 36");
+ var ans = table * 10;
+ document.write("<br> 4 X 10= 40");
+ document.write("<br>");
+ 
+ var c = 25;
+ var farenheit = ( c * 9 / 5 + 32);
+ console.log(farenheit);
+ document.write("<br><h1><b> Temperature</b></h1>")
+ document.write("<br> 25 °C is 77 °F");
+ var f = 70;
+ var centigrade = ( f - 32 ) * 5 / 9;
+ console.log(centigrade);
+ document.write("<br> 70°F is 21.11111111°C");
+ var item1 = 600;
+ var quantity = 3;
+ var item1cost= 650 * 3;
+ console.log(item1cost);
+ var item2 = 100;
+ var quantity2 = 7;
+ var item2cost = 100 * 7;
+ console.log(item2cost);
+ var shipping = 100;
+ var totalcost = item1cost + item2cost + shipping;
+
+ console.log(totalcost);
+ document.write("<br><h1><b>Shopping Cart</b></h1>");
+ document.write("<br> Price of item 1 is 650");
+ document.write("<br> Quantity of item 1 is 3");
+ document.write("<br> Price of item 2 is 100");
+ document.write("<br> Quantity of item 2 is 7");
+ document.write("<br> Shipping Charges is 100");
+ document.write("<br>")
+ document.write("<br> Total cost order is 2750");
+ var totalmarks = 980;
+ var markobtained = 804;
+ var percent = markobtained / totalmarks * 100;
+ var percent = 82.0408163265306;
+ document.write("<br><h1><b>Marks Sheet</b></h1>");
+ document.write("<br>");
+ document.write("Total marks: 980");
+ document.write("<br>Marks Obtained : 804");
+ document.write("<br>Percentage :82.0408163265306%");
+ var us$ = 10;
+ var pakrupee = 10 * 104.80
+ var pakrupee = 1048;
+ var riyal = 25;
+ var pakrs = 25 * 28;
+ var pakrs = 700 ;
+ var totalcurrency = 1048 + 700;
+ var totalcurrency = 1748;
+ document.write("<br><h1><b> Currency in PKR</b></h1>");
+ document.write("<br>");
+ document.write("Total Currency in PKR is 1748");
+ var year = 2016;
+ var birthyear = 1992;
+ var age = 2016 - 1992 ;
+ document.write("<br><h1><b>Age Calculator</b></h1>");
+ document.write("<br>");
+ document.write("<br> Current Year: 2016");
+ document.write("<br> Birth Year : 1992");
+ document.write("<br> Your Age : 24");
+ var radiusofcircle = 20;
+var circumference = 2 * 3.142 * 20;
+ var circumference = 125.679999999999999;
+ var areaofcircle = 3.142 * 20 * 20;
+ var areaofcircle = 1256.8;
+ document.write("<br><h1><b>The Geometrizer</b></h1>");
+ document.write("<br>");
+ document.write("<br>Radius of a Circle :20");
+ document.write("<br>The circumference is :125.679999999999");
+ document.write("<br>The area is 1256.8");
+
+var favouritesnack = "chocolatechip";
+var currentage = 15;
+var maximumage= 65;
+var perday = 3;
+var amount = (65 -15) * 3;
+console.log(amount);
+document.write("<br><h1><b>The LifeTime Supply Calculator</b></h1>");
+document.write("<br>");
+document.write("<br>Favourite Snack :chocolate chip");
+document.write("<br>Current Age:15");
+document.write("<br>Estimated maximum age:65");
+document.write("<br>Amount of snacks per day:3");
+document.write("<br>You will need 150 chocolate chip to last you until the ripe old age of 65");
+
+var g = 10;
+var h = ++g;
+console.log(h);
+document.write("<br><h5>Result:</h5>");
+document.write("<br>The value of g is 10");
+document.write("<br>-----------------------");
+document.write("<br>");
+document.write("<br> The value of ++g is 11");
+document.write("<br>Now the value of g is 11");
+document.write("<br");
+// chapter 6- 9 \\
+var g = 11;
+var h = g++;
+console.log(h);
+document.write("The value of g is 11");
+document.write("Now the value of g++ is 11");
+document.write("br");
+var g = 11;
+var h = --g;
+console.log(h);
+document.write("The value of --g is 10");
+document.write("Now the value of g is 10");
+document.write("br");
+var g = 11;
+var h = g--;
+console.log(h);
+document.write("<br>The value of g-- is 11");
+document.write("<br> Now the value of g is 11");
+document.write("<br>");
+var j = 2;
+var k = 1;
+var l = --j - --k;
+console.log(l);
+var l = ++k +k--;
+console.log(l);
+var j = 2;
+var k = 1;
+var result = --j - --k + ++k + k--;
+console.log(result);
+document.write("<br> j is 2");
+document.write("<br> k is 1");
+document.write("<br>result is 3");
+document.write("<br>");
+var username = prompt("hi! ENter your name");
+var table5 = prompt ("enter table","5 X 1 = 5");
+// chapter 9 -11 \\
+var cityname = Karachi;
+var Karachi = true;
+if (Karachi == true) {
+    alert("Welcome to city of Lights");
+}
+else {
+ alert("not interested");
+}
+var gender = "male" ;
+var gendr = "female" ;
+male = true ;
+female = true ;
+if (male == true) {
+    alert("good morning sir"); 
+} else if ( female == true) {
+    alert("Good morning Maam");
+}
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
